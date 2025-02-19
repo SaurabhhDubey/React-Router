f049054ee3c440170214bf7dee8fc3a6fe236b7a
Saurabh Dubey <saurabhdubey3712@gmail.com>
2025-02-19 10:48:18 +0530

    image

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
               <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
                   <div className="md:5/12 lg:w-5/12">
                       <img
-                          src="https://tailus.io/sources/blocks/left-image/preview/images/startup.png"
+                          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS37A8z-QLjb6a0h8pwoy1ivqIcYuGrcHn2LQ&s"
                           alt="image"
                       />
                   </div>
