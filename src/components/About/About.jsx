50fc63058bfcd89d888c92bb7de035419caa1b1d
Saurabh Dubey <saurabhdubey3712@gmail.com>
2025-02-21 22:44:27 +0530

    final

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
               <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
                   <div className="md:5/12 lg:w-5/12">
                       <img
-                          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS37A8z-QLjb6a0h8pwoy1ivqIcYuGrcHn2LQ&s"
+                          src="https://www.wibi.co.za/assets/startup.600859df.png"
                           alt="image"
                       />
                   </div>
